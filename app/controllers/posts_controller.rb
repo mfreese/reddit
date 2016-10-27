47528b91c93e0886da4210957e0f7a8da69a2735
Michael Freese <mrfreese1@gmail.com>
2016-10-27 15:35:08 -0400

    added seed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+  def index
+    @post = Post.order(vote: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post.name = params [:post][:name]
+    @post.link = params [:post][:link]
+    @post.vote = 1
+    redirect_to
+  end
+
+
+  def show
+  end
+
+
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+15.times do
+  Post.create!(
+  name: Faker::Superhero.name,
+  link: Faker::Internet.url,
+  vote: rand(3..100)
+
+  )
+
+end
