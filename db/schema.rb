f71466c7dec6a7763089f37bab2dca87331b6d60
Michael Freese <mrfreese1@gmail.com>
2016-10-28 15:36:12 -0400

    Adjusts for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161028183125) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text     "name"
     t.integer  "vote"
